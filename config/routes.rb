d7619299ee1b0c349e5473344af8fc0f26f3778f
enoch232 <enoch232@gmail.com>
2017-01-27 23:53:42 -0700

    add react

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'homes#home'
+  get 'homes/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
